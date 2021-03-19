066362e7855b5d3c612de9b3f026b608c166c9ea
petertran98 <Petertran198@gmail.com>
2021-03-19 00:13:52 -0500

    added custom hook to clean up code by displaying section array objects in another file

--- src/components/directory/Directory.jsx
+++ src/components/directory/Directory.jsx
@@ -1,43 +1,11 @@
 import React, {useState} from 'react'
+import useSectionList from './useSectionList';
 import MenuItem from '../menu-item/MenuItem';
 import './directory.scss'
 export const Directory = () => {
-    const [sections, setSections] = useState(
-        [
-            {
-              title: 'Hats',
-              imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-              id: 1,
-              linkUrl: 'hats'
-            },
-            {
-              title: 'Jackets',
-              imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-              id: 2,
-              linkUrl: ''
-            },
-            {
-              title: 'Sneakers',
-              imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-              id: 3,
-              linkUrl: ''
-            },
-            {
-              title: 'Womens',
-              imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-              size: 'large',
-              id: 4,
-              linkUrl: ''
-            },
-            {
-              title: 'Mens',
-              imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-              size: 'large',
-              id: 5,
-              linkUrl: ''
-            }
-          ]
-    );
+    //use custom hook to shorten code and not display the array of section objects, aka hats, mens, women
+    const [sections, setSections] = useSectionList();
+    
     return (
         <div className="directory-menu">
             {
